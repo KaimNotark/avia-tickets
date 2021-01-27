6b80d3f3b59e22b433f12fcbeeaf3db5c465bd91
KaimNotark <kaimnotark@yandex.ru>
2021-01-27 11:06:21 +0800

    add form methods for work with server

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,37 @@
 import '../css/style.css';
 import './plugins';
 import locations from './store/location.js';
+import formUI from './views/form.js';
+import currencyUI from './views/currency.js';
 
-locations.init().then(res => {
-  console.log(res);
-  console.log(locations);
-  console.log(locations.getCitiesByCountryCode('PE'));
+document.addEventListener('DOMContentLoaded', () => {
+  initApp();
+  const form = formUI.form;
 
-});
+  // Events
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onFormSubmit();
+  })
+
+  // Handlers
+  async function initApp() {
+    await locations.init();
+    formUI.setAutocompleteData(locations.shortCitiesList);
+  }
+
+  async function onFormSubmit() {
+    const origin = locations.getCityCodeByKey(formUI.originValue);
+    const destination = locations.getCityCodeByKey(formUI.destinationValue);
+    const depart_date = formUI.departDateValue;
+    const return_date = formUI.returnDateValue;
+    const currency = currencyUI.currencyValue;
+
+    console.log(origin, destination, depart_date, return_date);
+
+    await locations.fetchTickets({
+      origin, destination, depart_date, return_date, currency,
+    });
+  }
+})
+

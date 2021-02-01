f2791d7dc56e5ab01ea161c573a4f328b3e16e66
KaimNotark <KaimNotark@yandex.ru>
2021-02-01 23:42:47 +0800

    done adding favorite tickets to the favorite array

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,48 @@
-import '../css/style.css';
-import './plugins';
-import locations from './store/location.js';
-import { allFavorites } from './store/favorites.js';
-import formUI from './views/form.js';
-import currencyUI from './views/currency.js';
-import ticketsUI from './views/tickets.js';
-import favoriteTicketsUI from './views/favorites.js';
+import "../css/style.css";
+import "./plugins";
+import locations from "./store/location.js";
+import { allFavorites } from "./store/favorites.js";
+import formUI from "./views/form.js";
+import currencyUI from "./views/currency.js";
+import ticketsUI from "./views/tickets.js";
+import favoriteTicketsUI from "./views/favorites.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   initApp();
+  let ticketId = 0;
 
   // Elements
   const form = formUI.form;
-  const favoriteButton = document.getElementById('favorite');
-  const listContainer = document.querySelector('.tickets-sections .container .row');
+  const favoriteButton = document.getElementById("favorite");
+  const listContainer = document.querySelector(
+    ".tickets-sections .container .row"
+  );
 
   // Events
-  form.addEventListener('submit', (e) => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     onFormSubmit();
   });
 
-  favoriteButton.addEventListener('click', showFavoritesTickets);
-  listContainer.addEventListener('click', addIntoFavoritesList);
+  favoriteButton.addEventListener("click", showFavoritesTickets);
+  listContainer.addEventListener("click", addIntoFavoritesList);
 
   // Handlers
   function addIntoFavoritesList({ target }) {
-
-    if (target.classList.contains('add-favorite')) {
-      const parent = target.closest('[id]');
-      const id = parent.dataset.ticketsId;
+    if (target.classList.contains("add-favorite")) {
+      const parent = target.closest("[id]");
+      this.ticketId = parent.dataset.ticketsId;
     }
 
-    // console.log(this.id);
-    // const arrOfTickets = locations.lastSearch;
-    // console.log(arrOfTickets[this.id]);
+    const arrOfTickets = locations.lastSearch;
+    const selectedCard = arrOfTickets[this.ticketId];
 
-    // const selectedCard = locations.lastSearch.filter(e => { e.id === 2 });
-    // console.log("selectedCard", selectedCard);
-
+    allFavorites.push(selectedCard);
   }
 
   function showFavoritesTickets() {
     favoriteTicketsUI.renderFavoriteTickets(allFavorites);
-  };
+  }
 
   async function initApp() {
     await locations.init();
@@ -51,7 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   async function onFormSubmit() {
-
     const origin = locations.getCityCodeByKey(formUI.originValue);
     const destination = locations.getCityCodeByKey(formUI.destinationValue);
     const depart_date = formUI.departDateValue;
